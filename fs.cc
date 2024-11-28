d290bb4b4a12ab38ef7b51de44b23ed7b40a1733
Isaque Beirith <isaqueflorianobeirith@gmail.com>
2024-11-28 20:12:45 -0300

    debug (agora sim)

--- fs.cc
+++ fs.cc
@@ -1,14 +1,22 @@
 #include "fs.h"
+#include <cstring>
 
 int INE5412_FS::fs_format()
 {
 	return 0;
 }
 
+// Ok
 void INE5412_FS::fs_debug()
 {
+	// Variáveis auxiliares
 	union fs_block block;
+	fs_inode inode;
+	int direct;
+	int indirect;
+	int pointers;
 
+	// Inicialmente, lê o Superbloco (bloco 0) e exibe suas respectivas informações
 	disk->read(0, block.data);
 
 	cout << "superblock:\n";
@@ -17,42 +25,53 @@ void INE5412_FS::fs_debug()
 	cout << "    " << block.super.ninodeblocks << " inode blocks\n";
 	cout << "    " << block.super.ninodes << " inodes\n";
 
+	// Em seguida, lê os blocos de Inode
 	for (int i = 0; i < block.super.ninodeblocks; i++) 
 	{
-		disk->read(i + 1, block.data);
-
 		for (int j = 0; j < INODES_PER_BLOCK; j++)
 		{
-			if (block.inode[j].isvalid)
+			// O bloco é sempre lido novamente a cada Inode percorrido,
+			// por conta da possibilidade de leitura de um bloco indireto,
+			// que irá subscrever os dados do bloco de inode em block.data
+			disk->read(i + 1, block.data);
+			inode = block.inode[j];
+
+			if (inode.isvalid)
 			{
 				cout << "inode " << i * INODES_PER_BLOCK + j << ":\n";
-				cout << "    size: " << block.inode[j].size << " bytes\n";
+				cout << "    size: " << inode.size << " bytes\n";
+
 				cout << "    direct blocks:";
 				for (int k = 0; k < POINTERS_PER_INODE; k++)
 				{
-					if (block.inode[j].direct[k] != 0)
+					direct = inode.direct[k];
+					if (direct != 0)
 					{
-						cout << " " << block.inode[j].direct[k];
+						cout << " " << direct;
 					}
 				}
 				cout << "\n";
-				if (block.inode[j].indirect != 0)
+
+				indirect = inode.indirect;
+				if (indirect != 0)
 				{
-					cout << "    indirect block: " << block.inode[j].indirect << "\n";
+					cout << "    indirect block: " << indirect << "\n";
 					cout << "    indirect data blocks:";
+
+					// Lê o bloco indireto e exibe seus ponteiros
+					disk->read(indirect, block.data);
 					for (int k = 0; k < POINTERS_PER_BLOCK; k++)
 					{
-						if (block.pointers[k] != 0)
+						pointers = block.pointers[k];
+						if (pointers != 0)
 						{
-							cout << " " << block.pointers[k];
+							cout << " " << pointers;
 						}
 					}
 					cout << "\n";
 				}
 			}
-
 		}
-
 	}
 }
 
@@ -85,3 +104,13 @@ int INE5412_FS::fs_write(int inumber, const char *data, int length, int offset)
 {
 	return 0;
 }
+
+void INE5412_FS::inode_load(int inumber, class fs_inode *inode)
+{
+	cout << "Inode Load";
+}
+
+void INE5412_FS::inode_save(int inumber, class fs_inode *inode)
+{
+	cout << "Inode Save";
+} 
